3d05c6d9b9c0509fe6434f459f4aca9b39ea8a52
rebecca <rlerer3@gmail.com>
2024-03-21 19:25:00 -0400

    finished practice

---
+++ app.js
@@ -0,0 +1,106 @@
+// let launchReady = false;
+// let fuelLevel = 17000;
+
+// if (fuelLevel >= 20000) {
+//    console.log('Fuel level cleared.');
+//    launchReady = true;
+// } else {
+//    console.log('WARNING: Insufficient fuel!');
+//    launchReady = false;
+// }
+
+
+// // #2: The next block of code hides two syntax errors. Run the code as-is to find the mistakes.
+// let launchReady = false;
+// let crewStatus = true;
+// let computerStatus = 'green';
+
+// if (crewStatus && computerStatus === 'green') {
+//    console.log('Crew & computer cleared.');
+//    launchReady = true;
+// } else {
+//    console.log('WARNING: Crew or computer not ready!');
+//    launchReady = false;
+// }
+
+// if (launchReady) {
+//    console.log("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...");
+//    console.log("Fed parrot...");
+//    console.log("Ignition...");
+//    console.log("Liftoff!");
+// } else {
+//    console.log("Launch scrubbed.");
+// }
+
+
+
+
+// // #4: The next block of code hides four syntax errors. Run the code as-is to find the mistakes.
+// var numKids  = 5;
+// var partner  = 'David Beckham';
+// var geoLocation = 'Costa Rica';
+// var jobTitle = 'web developer';
+
+// var Future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ', and married to ' +
+//    partner + ' ' + ' with ' + numKids + ' kids.';
+// console.log(Future);
+
+// #5: The next block of code hides one logical error. Run the code as-is to find the mistakes.
+// var birthYear = 1984;
+// var futureYear  = 2012;
+// var age;
+// age  = futureYear - birthYear;
+// console.log(`I will be either  ${age}  or ${age -1}` );
+
+// #6: The next block of code hides one logical error. Run the code as-is to find the mistakes.
+// // Multiplication tables
+
+// var multiplier = 9;
+// for (var i = 0; i <= 10; i++) {
+//     var result = multiplier * i;
+//     console.log(multiplier + ' * ' + i + ' = ' + result);
+// }
+
+
+// #7: The next block of code hides three  logical error. Run the code as-is to find the mistakes.
+// //The for loop will iterate from 0 to 20, but should stop at 18 then continue 
+
+// for (var i = 0; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i + ' is even');
+//     } else {
+//         console.log(i + ' is odd');
+//     }
+// }
+// #8: The next block of code hides three  logical error. Run the code as-is to find the mistakes.
+// a = 5;
+// b = 6;
+// c = a + b;
+// while( a < b) {
+// console.log(c);
+// break;
+// }
+
+// #9: The next block of code hides two logical errors. Run the code as-is to find the mistakes.
+// let text = "";
+// let i = 0;
+// do {
+//   text += i + "<br>";
+//   i++;
+// }
+// while (i < 5);
+
+// #10: The next block of code hides three  logical error. Run the code as-is to find the mistakes.
+// const score = 45
+
+// const scoreRating =
+//   score > 70
+//     ? "Excellent"
+//     : score > 50
+//     ? "Average"
+//     : score > 40
+//     ? "Fair"
+//     : "Do better"
+
+// console.log(scoreRating)
+
